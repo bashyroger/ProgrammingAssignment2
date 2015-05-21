b787eee929f7dd5ab6aa07607b8b36a56b3133e3
Rogier Werschkull <rogierwerschkull@gmail.com>
2015-05-21 11:26:31 +0200

    Coursera R programming assignment week 2
    Assginment for week 2. I added an extra check to check whether the
    matrix is square

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## This function creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+    invm <- NULL
+    
+    ##function to cache matrix and inverse in different environment
+    set <- function(y) {
+        x <<- y
+        invm <<- NULL
+    }
+    
+    ## Functions to get input maxtix, set and get the inverse matrix
+    get <- function() x
+    setinverse <- function(inverse) invm <<- inverse
+    getinverse <- function() invm
+    
+    ##return as list
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
+    
 }
 
 
-## Write a short comment describing this function
+##This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
+cacheSolve <- function(x, ...) { 
+    
+            ##check whether inverse is already calculated, if so, get from cache
+            invm <- x$getinverse()
+            if(!is.null(invm)) {
+                message("getting data from the cache...")
+                return(invm)
+            }
+            
+            ##get input matrix
+            data <- x$get()
+            
+            ##not needed for assignment, but check whether matrix is square or not!
+            if(nrow(data)==ncol(data)) {         
+                ##If square, then calculate inverse
+                invm <- solve(data, ...)            
+                x$setinverse(invm)
+                return(invm)
+            
+            } else {
+                #If NOT square, return message and NA
+                message("Matrix is not square, Cannot calculate inverse")
+                return(NA)
+                
+            } 
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
 }
